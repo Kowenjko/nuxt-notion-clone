76f31adbf3418b4cfc5427a2ffeeff28e762af5e
Kowenjko <ortoswt@gmail.com>
2023-12-04 14:00:12 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+	title: 'Genius',
+	meta: [{ name: 'description', content: 'AI Platform' }],
+})
+</script>
+
+<template>
+	<suspense>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</suspense>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+	<h1 class="text-4xl font-bold text-center">Home page</h1>
+
+	<div class="flex justify-center mt-10">
+		<Button variant="purple">Test</Button>
+	</div>
+</template>
+
+<style scoped></style>
